6fc2b88747c6d9af8a2ed229318bb5f67c002268
SBcodework <50241115+SBcodework@users.noreply.github.com>
2019-08-30 04:07:29 -0400

    Separation of constants
    Separated the naming and setting of constants from "main.cpp" to "Constants.cpp" and "Constants.h".

---
+++ Constants.h
@@ -0,0 +1,45 @@
+/**
+A BSD 2-Clause License is in use here. See LICENSE.md for license information.
+Copyright (c) 2019, SBcodework
+All rights reserved.
+**/
+
+#ifndef CONSTANTS_H_INCLUDED
+#define CONSTANTS_H_INCLUDED
+
+#include "SDL.h"
+
+namespace C
+{
+     extern char title[];
+     extern int windowX;
+     extern int windowY;
+     extern int windowW;
+     extern int windowH;
+     extern int FPS;
+     extern Uint32 windowFlags;
+     extern Uint32 rendererFlags;
+
+     extern Uint8 bgColors[];
+     extern Uint8 gameObjectColors[];
+
+     extern SDL_Keycode leftPaddleKeys[2];
+     extern SDL_Keycode rightPaddleKeys[2];
+
+    // May be calculated from previous constants
+     extern int paddlePadding;
+     extern int paddleH;
+     extern int paddleW;
+     extern int paddleY;
+     extern int ballW;
+     extern int ballPixPerSec;  /// Note: ball bounce currently depends in FPS being divisible by 60 int division
+     extern int paddlePixPerSec;
+
+     extern int leftPaddleDim[4]; // X,Y, W,H
+     extern int rightPaddleDim[4];
+     extern int ballDim[4];
+
+     extern int bounceBoundaryDim[4];  // If the ball is inside this box, it is not in the goal.
+}
+
+#endif // CONSTANTS_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -27,6 +27,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <vector>
 #include <time.h>
 
+#include "Constants.h"
+
 void initRect(int x, int y, int w, int h, SDL_Rect* output)
 {
     output->x = x;
@@ -43,39 +45,6 @@ void initRect(int* dimensions, SDL_Rect* output)
     output->w = dimensions[2];
     output->h = dimensions[3];
     return;
-}
-
-namespace C // Constants
-{
-    char title[] {"Pong!"};
-    int windowX = SDL_WINDOWPOS_UNDEFINED;
-    int windowY = SDL_WINDOWPOS_UNDEFINED;
-    int windowW = 1200;
-    int windowH = 800;
-    int FPS = 60;
-    Uint32 windowFlags = SDL_WINDOW_SHOWN;
-    Uint32 rendererFlags = SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC;
-
-    Uint8 bgColors[] {0,0,0,255};
-    Uint8 gameObjectColors[] {255,0,0,255};
-
-    SDL_Keycode leftPaddleKeys[2] {SDLK_q, SDLK_a};
-    SDL_Keycode rightPaddleKeys[2] {SDLK_o, SDLK_l};
-
-    // May be calculated from previous constants
-    int paddlePadding = 50;
-    int paddleH = 200;
-    int paddleW = 50;
-    int paddleY = (windowH/2) - (paddleH/2);
-    int ballW = 50;
-    int ballPixPerSec = 600;  /// Note: ball bounce currently depends in FPS being divisible by 60 int division
-    int paddlePixPerSec = 1000;
-
-    int leftPaddleDim[4] {paddlePadding, paddleY, paddleW, paddleH}; // X,Y, W,H
-    int rightPaddleDim[4] {windowW - paddlePadding - paddleW, paddleY, paddleW, paddleH};
-    int ballDim[4] {(windowW/2) - (ballW/2), (windowH/2) - (ballW/2), ballW, ballW};
-
-    int bounceBoundaryDim[4] {paddlePadding + paddleW, 0, windowW - (2*(paddlePadding - paddleW))};  // If the ball is inside this box, it is not in the goal.
 }
 
 void displayUpdateRects(SDL_Renderer* renderer, SDL_Rect** rects, int number)
